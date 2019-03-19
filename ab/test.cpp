82eb5f8f8e92c5c1c847c21f34f8fbdce640e28e
yoshinobc <pipbcmsi@gmail.com>
2019-03-19 13:51:55 +0900

    add 085a

---
+++ ab/test.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  string std;
+  cin >> std;
+  std[3] = '8';
+  cout << std << endl;
+  return 0;
+}
